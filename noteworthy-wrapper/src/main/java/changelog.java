66bf35e1e5f2053adb0090175bbc5dea53982c3f
Dejan Josifović <www.paranoidtimes@gmail.com>
2021-12-22 14:20:05 +0100

    Implement changelog command
    This command parses the changelog and extracts entries for the specified version range.

---
+++ noteworthy-wrapper/src/main/java/changelog.java
@@ -0,0 +1,43 @@
+import org.theparanoidtimes.noteworthywrapper.ChangelogDeltaGenerator;
+import org.theparanoidtimes.noteworthywrapper.Constants;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+
+import static org.theparanoidtimes.noteworthywrapper.Constants.CHANGELOG_PATH;
+
+@Command(name = "changelog",
+        description = "Extracts info from the Change log for the required version range.",
+        version = "Version 1.0 Noteworthy II Wrapper",
+        mixinStandardHelpOptions = true
+)
+public class changelog implements Callable<Integer> {
+
+    @Option(names = {"-vf", "--versionFrom"},
+            description = "Starting version / heading.",
+            required = true)
+    private String versionFrom;
+
+    @Option(names = {"-vt", "--versionTo"},
+            description = "End version / heading.")
+    private String versionTo;
+
+    @Option(names = {"-c", "--changelogLocation"},
+            description = "The location of the change log file. Default is the file from the repository.")
+    private String changelogLocation = CHANGELOG_PATH;
+
+    @Override
+    public Integer call() throws Exception {
+        var delta = ChangelogDeltaGenerator.generateChangelogDelta(Path.of(changelogLocation), versionFrom, versionTo);
+        System.out.println(delta);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        var resultCode = new CommandLine(new changelog()).execute(args);
+        System.exit(resultCode);
+    }
+}
--- noteworthy-wrapper/src/main/java/org/theparanoidtimes/noteworthywrapper/Constants.java
+++ noteworthy-wrapper/src/main/java/org/theparanoidtimes/noteworthywrapper/Constants.java
@@ -10,6 +10,8 @@ public final class Constants {
     public static final Path TOC_PATH = Path.of("NoteworthyII.toc");
     public static final Path README_PATH = Path.of("README.md");
     public static final Path LICENSE_PATH = Path.of("LICENSE.txt");
+    public static final String CHANGELOG_PATH = "doc/CHANGELOG.md";
+    public static final String CHANGELOG_HEADING_DELIMITER = "##";
     public static final String INSTALLATION_DIRECTORY_ENV_VAR = "WOWIL";
     public static final String INSTALL_LOCATION = "Interface/AddOns/NoteworthyII";
 }
